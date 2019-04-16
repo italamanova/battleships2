853c353e6d3384827b2a74d99a1262a88493755a
Iryna Talamanova <iryna.talamanova@gmail.com>
2019-04-17 00:21:14 +0200

    Game process works properly now

--- battleships.py
+++ battleships.py
@@ -1,15 +1,9 @@
 from random import randint
 
 from constants import UNKNOWN, HORIZONTAL, VERTICAL, MISS, SIZE, ALIVE, FLEET, HIT
-from helpers import ShipBoard, Board, Ship
+from helpers import ShipBoard, Board
 from utils import print_board
 
-# player_board = ShipBoard(10)
-# ship = Ship('koko', 5)
-# ship.set_parameters(row=0, col=0, orientation=VERTICAL)
-# player_board.add_ship(ship)
-#
-# ai_guesses_board = Board(10)
 
 player_status = ALIVE
 player_guesses = Board(SIZE)
@@ -20,8 +14,6 @@ ai_guesses = Board(SIZE)
 ai_board = ShipBoard(SIZE)
 
 
-
-
 def place_ships(board):
     for ship in FLEET:
         created_ship = board.create_random_ship(ship, FLEET[ship])
@@ -30,7 +22,7 @@ def place_ships(board):
 
 # AI
 # Guesses cell if
-def guess_cell(last_row, last_col, ai_guesses_board):
+def guess_cell(ai_guesses_board):
     busy = True
     while busy:
         row_to_check = randint(0, SIZE - 1)
@@ -138,8 +130,6 @@ def guess_cell_horizontal(hit_row, hit_col, ai_guesses_board):
 
 
 def game():
-    last_row = UNKNOWN
-    last_col = UNKNOWN
     last_hit_row = UNKNOWN
     last_hit_col = UNKNOWN
     target_ship = None
@@ -147,6 +137,8 @@ def game():
 
     ai_ships_left = len(FLEET)
     player_ships_left = len(FLEET)
+    player_target_ship = None
+    player_hits = []
 
     place_ships(player_board)
     place_ships(ai_board)
@@ -156,34 +148,55 @@ def game():
 
     print('Battleships')
     print_board(player_board, player_guesses, SIZE)
-    print('AI BOARD')
-    print_board(ai_board, ai_guesses, SIZE)
+    # print('AI BOARD')
+    # print_board(ai_board, ai_guesses, SIZE)
+
     while ai_ships_left and player_ships_left:
-        # _row = int(input("Which row?"))
-        # _col = int(input("Which column?"))
-        #
-        # # if the guess is not legal
-        # while not player_guesses.is_cell_on_board(_row, _col):
-        #     print("Incorrect input, please try again")
-        #     _row = int(input("Which row?"))
-        #     _col = int(input("Which column?"))
-        #
-        # if not ai_board.is_cell_water(_row, _col):
-        #     if ai_ships_left:
-        #         print('Hit!')
-        #         player_guesses.board[_row][_col] = HIT
-        #     #     check ship sunk
-        #     else:
-        #         player_guesses.board[_row][_col] = HIT
-        #         print('You win!')
-        #         break
-        # else:
-        #     print("Missed!")
-        #     player_guesses.board[_row][_col] = MISS
+        _row = int(input("Which row?"))
+        _col = int(input("Which column?"))
+
+        # if the guess is not legal
+        while not player_guesses.is_cell_on_board(_row, _col):
+            print("Incorrect input, please try again")
+            _row = int(input("Which row?"))
+            _col = int(input("Which column?"))
+
+        player_hit = not ai_board.is_cell_water(_row, _col)
+        while player_hit:
+            player_guesses.board[_row][_col] = HIT
+            if ai_ships_left:
+                print('Hit!')
+                player_hits.append((_row, _col))
+                # check if any ai ship is sunk
+                if not player_target_ship:
+                    player_target_ship = ai_board.get_hit_ship(_row, _col)
+                else:
+                    is_ai_ship_sunk = player_target_ship.is_sunk(player_hits)
+                    print('SHIP', player_target_ship.coordinates)
+                    print('PLAYER HITS', player_hits)
+                    if is_ai_ship_sunk:
+                        player_hits = []
+                        ai_ships_left -= 1
+                        print('AI SHIP SUNK')
+
+                print_board(player_board, player_guesses, SIZE)
+
+                _row = int(input("Which row?"))
+                _col = int(input("Which column?"))
+
+                player_hit = not ai_board.is_cell_water(_row, _col)
+            else:
+                print('You win!')
+                break
+        print("Miss!")
+        player_guesses.board[_row][_col] = MISS
+
+        print_board(player_board, player_guesses, SIZE)
+
 
         # AI turn
         if last_hit_row == UNKNOWN:
-            guessed_row, guessed_col = guess_cell(last_row, last_col, ai_guesses)
+            guessed_row, guessed_col = guess_cell(ai_guesses)
         else:
             guessed_row, guessed_col = guess_cell_after_hit(last_hit_row, last_hit_col, ai_guesses)
 
@@ -200,7 +213,6 @@ def game():
             else:
                 is_ship_sunk = target_ship.is_sunk(hits)
                 if is_ship_sunk:
-
                     last_hit_row = UNKNOWN
                     last_hit_col = UNKNOWN
                     hits = []
@@ -209,28 +221,22 @@ def game():
                     player_ships_left -= 1
             if player_ships_left:
                 if last_hit_row == UNKNOWN:
-                    guessed_row, guessed_col = guess_cell(last_row, last_col, ai_guesses)
+                    guessed_row, guessed_col = guess_cell(ai_guesses)
                 else:
                     guessed_row, guessed_col = guess_cell_after_hit(last_hit_row, last_hit_col, ai_guesses)
             else:
                 print('AI WIN!')
                 break
-
-            last_row = guessed_row
-            last_col = guessed_col
-            hit = player_board.is_hit(last_row, last_col, ai_guesses.board)
-        last_row = guessed_row
-        last_col = guessed_col
+            hit = player_board.is_hit(guessed_row, guessed_col, ai_guesses.board)
 
         player_board.board[guessed_row][guessed_col] = MISS
         ai_guesses.board[guessed_row][guessed_col] = MISS
 
         print('----------------')
-        print('ME')
+        print('AI TURN')
         print_board(player_board, player_guesses, SIZE)
-        print('AI BOARD')
-        print_board(ai_board, ai_guesses, SIZE)
-        print('----------------')
+        # print('AI BOARD')
+        # print_board(ai_board, ai_guesses, SIZE)
 
 
 
